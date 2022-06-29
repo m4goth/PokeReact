bed3e61efcea6156604374800fe50a5ef086e1e3
Anderson Souza <andersom.eugenio@gmail.com>
2022-06-29 16:28:24 -0300

    removido imports nao usados

--- src/components/PokeDetail/PokeDetail.jsx
+++ src/components/PokeDetail/PokeDetail.jsx
@@ -11,7 +11,7 @@ import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
+// import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 
