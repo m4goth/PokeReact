5f9b2d8e2b8e80566a9e65b07c87098120772f46
m4goth <matheusdealmeidasilveira@gmail.com>
2022-07-04 18:40:30 -0300

    refatorado pageListaPokemon

--- src/pages/PageListaPokemon/PageListaPokemon.jsx
+++ src/pages/PageListaPokemon/PageListaPokemon.jsx
@@ -22,8 +22,6 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
 
 import { Icon } from '@iconify/react';
 
