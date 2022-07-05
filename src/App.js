e15277a8baa6de9d4048aedee9f6c2ce333cd29c
m4goth <matheusdealmeidasilveira@gmail.com>
2022-07-05 00:30:33 -0300

    organizando idéia

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import "./App.css";
 
-import Button from '@mui/material/Button'
+import PageDrawer from "./pages/PageDrawer/PageDrawer"
 import PageBuscaPokemon from "./pages/PageBuscaPokemon/PageBuscaPokemon"
 import PageListaPokemon from "./pages/PageListaPokemon/PageListaPokemon"
+
 
 import {
   BrowserRouter as Router,
@@ -16,32 +17,34 @@ const App = () => {
   const [page, setPage] = useState(1);
   let pageRender;
   if (page === 1) {
-    pageRender = <PageListaPokemon />
+    pageRender = <PageDrawer />
 
   } else if (page === 2) {
     pageRender =
+      <PageListaPokemon />
+  } else if (page === 3) {
+    pageRender =
       <PageBuscaPokemon />
-
   }
 
   return (
     <Router>
       <ul>
         <li>
-          <Link to="/">Lista de Pokemons</Link>
+          <Link to="/busca-de-pokemons">Busca de Pokemons</Link>
         </li>
         <li>
-          <Link to="/busca-de-pokemons">Busca de Pokemons</Link>
+          <Link to="/">Lista de Pokemons</Link>
         </li>
 
       </ul>
       <div className="App" >
         {/* <img alt="" src="https://logodownload.org/wp-content/uploads/2017/08/pokemon-logo-0.png" /> */}
         <Routes>
-          <Route exact path="/" element={<PageListaPokemon />}>
+          <Route path="/busca-de-pokemons" element={<PageBuscaPokemon />}>
 
           </Route>
-          <Route path="/busca-de-pokemons" element={<PageBuscaPokemon />}>
+          <Route exact path="/" element={<PageListaPokemon />}>
 
           </Route>
         </Routes>
